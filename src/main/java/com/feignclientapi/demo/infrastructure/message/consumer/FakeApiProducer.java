d2519efcddd76c453a1487f0c185bfaafac7a901
Thiago Teixeira <thiago.rtf@gmail.com>
2024-09-26 11:04:41 -0300

    add kafka implementation

--- src/main/java/com/feignclientapi/demo/infrastructure/message/consumer/FakeApiProducer.java
+++ src/main/java/com/feignclientapi/demo/infrastructure/message/consumer/FakeApiProducer.java
@@ -1,7 +1,6 @@
 package com.feignclientapi.demo.infrastructure.message.consumer;
 
 import com.feignclientapi.demo.infrastructure.exception.BusinessException;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
